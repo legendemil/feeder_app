035d6d71c40a6ba35920849ad40a5a02035c1136
Emil Pausz <emilpausz@gmail.com>
2016-05-04 19:14:32 +0200

    add header karmienie component

--- app/components/mainArea/karmienie.component.js
+++ app/components/mainArea/karmienie.component.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// import HeaderKarmienie
+import HeaderKarmienie from './karmienie/header.component';
 
 export default class Karmienie extends React.Component {
 	constructor(){
@@ -9,7 +11,7 @@ export default class Karmienie extends React.Component {
 	render(){
 		return (
 			<div className='col-md-12 main-area-content' id='karmienie'>
-				Karmienie
+				<HeaderKarmienie />
 			</div>
 		);
 	}
---
+++ app/components/mainArea/karmienie/header.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default class HeaderKarmienie extends React.Component {
+	constructor(){
+		super();
+	}
+
+	render(){
+		return (
+			<h1  id='karmienie-header'>
+				Twoje Zwierzęta
+			</h1>
+		);
+	}
+}
